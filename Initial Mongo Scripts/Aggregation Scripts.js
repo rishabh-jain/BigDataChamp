8d06d77502756d2b6a249baf08a85bd88dcc5879
rishabh-jain <rishabh-jain@users.noreply.github.com>
2014-01-10 16:21:26 +0530

    Updated Aggregation Script with script on User ID

--- Initial Mongo Scripts/Aggregation Scripts.js
+++ Initial Mongo Scripts/Aggregation Scripts.js
@@ -20,3 +20,93 @@ db.ItemEntityWithDescription.aggregate([
 /* Aggregate and get all the Titles of the various orders by a particular buyer*/
 db.OrderEntity.aggregate([
 	{ $project : { BuyerID : "$Order.BuyerUserID", ItemArray : "$Order.TransactionArray", CityName : "$Order.ShippingAddress.CityName", StateName : "$Order.ShippingAddress.StateOrProvince", Country : "$Order.ShippingAddress.CountryName" } }, { $unwind : "$ItemArray" }, { $group : { _id : "$BuyerID", items : { $addToSet : "$ItemArray.Item.Title" } } } ])
+
+
+
+/* Aggregate and get the data based on user id from item sold.*/
+var cur = db.ItemSold.aggregate(
+    { $project : 
+        {
+            "TimeStamp" : "$Payload.TimeStamp", 
+            "ItemID" : "$Payload.Item.ItemID", 
+            "ItemURL" : "$Payload.Item.ListingDetails.ViewItemURL", 
+            "ItemCategory" : "$Payload.Item.PrimaryCategory.CategoryName", 
+            "ItemQuantity" : "$Payload.Item.Quantity", 
+            "BidCount" : "$Payload.Item.SellingStatus.BidCount", 
+            "BuyerEmail" : "$Payload.Item.SellingStatus.HighBidder.Email", 
+            "BuyerID" : "$Payload.Item.SellingStatus.HighBidder.UserID", 
+            "QuantityPurchased" : "$Payload.Item.SellingStatus.QuantitySold", 
+            "ItemTitle" : "$Payload.Item.Title", 
+            "ItemSpecifics" : "$Payload.Item.ItemSpecifics" 
+        }
+     }, 
+     { $group : 
+         { 
+             _id : "$BuyerID", 
+             Email : { $first :  "$BuyerEmail" }, 
+             Items : 
+               { 
+                   $addToSet : 
+                      { 
+						 "ID" : "$ItemID", 
+						 "Title" : "$ItemTitle", 
+						 "TimeStamp" : "$TimeStamp", 
+						 "URL" : "$ItemURL", 
+						 "Category" : "$ItemCategory", 
+						 "Quantity" : "$ItemQuantity", 
+						 "BidCount" : "$BidCount", 
+						 "QuantityPurchased" : "$QuantityPurchased", 
+						 "Specifics" : "$ItemSpecifics"
+                       }
+               }
+         }
+      } );
+
+      print(cur.result);
+	  
+
+
+/* Aggregate and get the data based on user id from fixed price transaction.*/
+var cur = db.FixedPriceTransaction.aggregate(
+    { $project : 
+        {
+            "TimeStamp" : "$Payload.TimeStamp", 
+            "ItemID" : "$Payload.Item.ItemID", 
+            "ItemURL" : "$Payload.Item.ListingDetails.ViewItemURL", 
+            "ItemCategory" : "$Payload.Item.PrimaryCategory.CategoryName", 
+            "ItemQuantity" : "$Payload.Item.Quantity", 
+            "TransactionArray" : "$Payload.TransactionArray",
+            "ItemTitle" : "$Payload.Item.Title", 
+            "ItemSpecifics" : "$Payload.Item.ItemSpecifics" 
+        }
+     }, 
+     {
+       $unwind : "$TransactionArray"
+     },
+     { $group : 
+         { 
+             _id : "$TransactionArray.Buyer.UserID", 
+             Email : { $first : "$TransactionArray.Buyer.Email" }, 
+             Items : 
+               { 
+                   $addToSet : 
+                      { 
+						 "ID" : "$ItemID", 
+						 "Title" : "$ItemTitle", 
+						 "TimeStamp" : "$TimeStamp", 
+						 "URL" : "$ItemURL", 
+						 "Category" : "$ItemCategory", 
+						 "Quantity" : "$ItemQuantity",
+						 "QuantityPurchased" : "$TransactionArray.QuantityPurchased", 
+						 "Specifics" : "$ItemSpecifics"
+                       }
+               }
+         }
+      } );
+
+      print(cur.result);
+	  
+	  
+	  
+	  
+	  
